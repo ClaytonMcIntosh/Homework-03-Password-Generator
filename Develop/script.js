51094468cef8f6ce23d00258faacd680795322d5
Clay <claymcintosh@gmail.com>
2021-12-18 13:05:26 +1000

    Added simple if/else loop to the generatePassword function. Testing alerts, prompts and confirm with the console to set up how to page works. Next I need to work out how the password will actually generate from these inputs.

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,53 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//function to generate the password.
+function generatePassword(){
+  let passwordLength = prompt("Choose password length. \n (It must be between 8 and 128 characters.)")
+  if(isNaN(passwordLength)){
+    alert("That is not a number");
+    return generatePassword();
+  }
+  else if(passwordLength < 8 || passwordLength > 128){
+    alert("You must have between 8 and 128 characters.");
+    return generatePassword();
+  }
+   else{
+    console.log(passwordLength);
+  }
+  let lowerCaseReq = confirm("Do you require lowercase letters?")
+  console.log("Lowercase required =" + lowerCaseReq);
+  let upperCaseReq = confirm("Do you require uppercase letters?")
+  console.log("Uppercase required =" + upperCaseReq)
+  let numericReq = confirm("Do you require numbers?")
+  console.log("Numbers required =" + numericReq)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
