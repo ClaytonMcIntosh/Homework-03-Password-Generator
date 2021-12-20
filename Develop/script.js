98cb3d78eaadc1270813c3277795ec2fed5c7f6b
Clay <claymcintosh@gmail.com>
2021-12-20 14:22:18 +1000

    As far as I can tell, I have finished the program. It seems to work as it should. I will do some more testing, comment my code so it's clear, and deleat any unneeded code I have in comments down the bottom. I have also found that my comments were appearing on github before, I just was looking in the wrong place.

--- Develop/script.js
+++ Develop/script.js
@@ -17,17 +17,27 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
 
- refinedPass = passwordGet();
+  lengthP = lengthGet();
 
-  return refinedPass
+  stringP = stringGet(lengthP);
 
-}
+  firstPass = passGet(stringP);
+ 
+  refinedPass = passwordRefined(firstPass);
 
+  return refinedPass;
 
+ }
 
-function passwordGet(){
+let lowerCaseReq = false;
+let upperCaseReq = false;
+let numericReq = false;
+let specialReq = false;
+let passwordJ = false;
+let longStringName = "";
+let passwordLength = "";
 
-  longStringName = "";
+function lengthGet(){
   passwordLength = prompt("Choose password length. \n (It must be between 8 and 128 characters.)")
   if(isNaN(passwordLength)){
     alert("That is not a number");
@@ -37,39 +47,146 @@ function passwordGet(){
     alert("You must have between 8 and 128 characters.");
     return generatePassword();
   }
-  let lowerCaseReq = confirm("Do you require lowercase letters?")
+}
+
+function stringGet(){
+  longStringName = "";
+  lowerCaseReq = confirm("Do you require lowercase letters?");
     if(lowerCaseReq === true){
       longStringName = longStringName+"abcdefghijklmnopqrstuvwxyz";
     }
-  let upperCaseReq = confirm("Do you require uppercase letters?")
+  upperCaseReq = confirm("Do you require uppercase letters?");
     if(upperCaseReq === true){
       longStringName = longStringName+"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     }
-  let numericReq = confirm("Do you require numbers?")
+  numericReq = confirm("Do you require numbers?");
     if(numericReq === true){
       longStringName = longStringName+"0123456789";
     }
-  let specialReq = confirm("Do you require special characters?")
-  if(specialReq === true){
+  specialReq = confirm("Do you require special characters?");
+    if(specialReq === true){
     longStringName = longStringName+"!@,#$%&*{}/+=";
    }
+   console.log(longStringName);
+   return longStringName;
+  }
+
+function passGet(x){
 
   if (specialReq === false && numericReq === false && upperCaseReq === false && lowerCaseReq === false) {
     alert("Please choose some kind of character. I recommend choosing them all!");
-    return generatePassword();}
+    return stringGet()
+  }
 
     else {
     function passJumb(length, chars) {
       var passwordJ = '';
       for (var i = length; i > 0; --i) passwordJ += chars[Math.floor(Math.random() * chars.length)];
       return passwordJ
-     }
+    }
     
-  pJumb = passJumb(passwordLength, longStringName);
-  return pJumb 
-  
+    pJumb = passJumb(passwordLength, x);
+    return pJumb ;
   }
-  }
+}
+
+function passwordRefined(x){
+
+  console.log(x);
+    if (lowerCaseReq === true && /[a-z]/.test(x) || lowerCaseReq === false){
+    console.log("aa");
+    }
+
+    else {
+      console.log("aaaaaaaaaaa")
+      y = passGet(longStringName);
+      passwordRefined(y);
+      return y;   
+    }
+    if (upperCaseReq === true && /[A-Z]/.test(x) || upperCaseReq === false){
+      console.log("AA");
+    }
+    else {
+      console.log("AAAAAAAAAA")
+      y = passGet(longStringName);
+      passwordRefined(y);
+      return y;   
+    }
+    if (numericReq === true && /[0-9]/.test(x) || numericReq === false){
+      console.log("11");
+       }
+    else {
+      console.log("111111111")
+      y = passGet(longStringName);
+      passwordRefined(y);
+      return y;   
+    }
+    if (specialReq === true && /[!@,#$%&*{}/+=]/.test(x) || specialReq === false){
+      console.log("**");
+      return x;
+       }
+    else {
+      console.log("!@,#$%&*{}/+=")
+      y = passGet(longStringName);
+      passwordRefined(y);
+      return y;   
+    }
+   
+   
+
+}
+
+
+
+
+// console.log(x);
+// if (lowerCaseReq === true && /[a-z]/.test(x)){
+// console.log("aa");
+// }
+// else if (lowerCaseReq === false) {
+//   console.log("aaNOTNEEDED");
+// return x;
+// }
+//  else {
+//   console.log("Woooooo")
+//   y = passGet(longStringName);
+//   passwordRefined(y);
+//   return y;
+// }
+
+
+
+
+
+
+
+// console.log(x);
+// if (lowerCaseReq === true && /[a-z]/.test(x)){
+// console.log("aa");
+// return x;
+// }
+// else {
+//   console.log("nup");
+//   x = false;
+//   return x;
+// }
+
+
+
+
+
+
+// if (x = false) {
+//   console.log("Yabba dabba do");
+//   firstPass = passGet(stringP);
+//   refinedPass = passwordRefined(firstPass);
+//   return refinedPass
+// }
+
+// else {
+//   console.log("Tick tock")
+//   return refinedPass
+// }
 
 
 
